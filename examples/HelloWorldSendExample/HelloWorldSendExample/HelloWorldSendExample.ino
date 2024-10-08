6fdcaf748e198d1cb62f2325454ba3029985d33b
vegibaby1974 <147331568+vegibaby1974@users.noreply.github.com>
2024-10-08 19:01:21 -0400

    Add files via upload

---
+++ examples/HelloWorldSendExample/HelloWorldSendExample/HelloWorldSendExample.ino
@@ -0,0 +1,12 @@
+#include <EspRightNow.h>
+Esp_Now_ESP32 espnow;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
