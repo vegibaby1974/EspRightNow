1bb3e3d26694700602a33ee0134e4b9eb922a27f
vegibaby1974 <147331568+vegibaby1974@users.noreply.github.com>
2025-07-10 11:14:36 -0400

    Update EspNowRecvBuffer.ino

--- examples/EspNowRecvBuffer/EspNowRecvBuffer.ino
+++ examples/EspNowRecvBuffer/EspNowRecvBuffer.ino
@@ -5,14 +5,20 @@ uint8_t mac[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //Replace with sending boa
 void setup() {
   // put your setup code here, to run once:
   espnow.begin((uint8_t*)mac);
-  Serial.begin(115200, SERIAL_8N1, RX, TX, false, 20000UL);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (espnow.cbuffer != "") {
-    Serial.print(espnow.cbuffer);
-    espnow.cbuffer = "";
+  //Old version
+  //if (espnow.cbuffer != "") {
+  //  Serial.print(espnow.cbuffer);
+  //  espnow.cbuffer = "";
+  //}
+  //delay(20);
+  //Now is:
+  if (espnow.available()) {
+    Serial.print(espnow.readString(false)) //readString param oneSecondDelay: if true, it will wait one second before responce like standard Serial.readString
   }
-  delay(20);
+  delay(10); //Small delay to prevent program from running too fast
 }
