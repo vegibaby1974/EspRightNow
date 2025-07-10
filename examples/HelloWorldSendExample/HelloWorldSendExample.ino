9ab48f5a3e86091b52b8b3b390d5468e2fee88fa
vegibaby1974 <147331568+vegibaby1974@users.noreply.github.com>
2025-07-10 11:11:57 -0400

    Update HelloWorldSendExample.ino

--- examples/HelloWorldSendExample/HelloWorldSendExample.ino
+++ examples/HelloWorldSendExample/HelloWorldSendExample.ino
@@ -1,14 +1,16 @@
 #include <EspRightNow.h>
 Esp_Now_ESP32 espnow;
-uint8_t mac[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00); //Replace with recieving board MAC address
+uint8_t mac[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00); //Replace with recieving board MAC address
 
 void setup() {
   // put your setup code here, to run once:
   espnow.begin((uint8_t*)mac);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  espnow.print(Hello World!!!); //Print a message
+  espnow.println("Hello World!!!"); //Print a message
+  Serial.println("Message sent");
   delay(5000); //5 second delay in between message
 }
